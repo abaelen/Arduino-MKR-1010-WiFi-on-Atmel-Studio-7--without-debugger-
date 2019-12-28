590c9c54175f4c19acd25fcab16d64b733e35fae
abaelen <44756098+abaelen@users.noreply.github.com>
2019-12-28 15:04:33 +0100

    Adding AS7 code

---
+++ ArduinoSketch9/ArduinoSketch9/ArduinoCore/library.cpp
@@ -0,0 +1,15 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 28/12/2019 12:21:21
+ * Author : Gebruiker
+ */ 
+
+#include "sam.h"
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
---
+++ ArduinoSketch9/ArduinoSketch9/Arduino_template_v1/Sketch.cpp
@@ -0,0 +1,21 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+void setup() {
+  // put your setup code here, to run once:
+	pinMode(6,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+	digitalWrite(6,HIGH);
+	delay(1000);
+	digitalWrite(6,LOW);
+	delay(1000);
+}
